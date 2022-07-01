ba8540a6181b97e79a1495ba79df35c1416645e3
Yofou <exodias1965@gmail.com>
2022-07-01 04:10:43 +0100

    feat: added left-hook & prettier

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,6 +1,4 @@
 import { createCookie } from "@remix-run/node";
 
 export type ThemeType = "dark" | "light"
-export const theme = createCookie("theme", {
-	maxAge: 60 * 60 * 24 * 30 * 12 
-})
+export const theme = createCookie("theme", { maxAge: 60 * 60 * 24 * 30 * 12 });
