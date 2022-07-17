1c4880083df63c70600f00905787e40c547cb791
Yofou <exodias1965@gmail.com>
2022-07-18 00:52:21 +0100

    feat: Replaced embla with react-spring & use geasture

--- app/components/dashboard/board.tsx
+++ app/components/dashboard/board.tsx
@@ -1,21 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "~/lib/store";
 import { Modal } from "../shared/modal";
 import { AddColumn } from "./add-column";
 import { Column } from "./column";
-import useEmblaCarousel from "embla-carousel-react";
 import { Task } from "./task";
 import { SubTask, Task as TaskType } from "@prisma/client";
 import { InnerTask } from "./inner-task";
 import { EditTask } from "./edit-task";
+import { useSpring, animated, easings } from "@react-spring/web";
+import { useDrag } from "@use-gesture/react";
 
 export const Board: React.FC = () => {
-	const [ref, embla] = useEmblaCarousel({
-		dragFree: true,
-		containScroll: "keepSnaps",
-	});
-
 	const [selectedTask, setSelectedTask] = useState<
 		(TaskType & { subtasks: SubTask[] }) | null
 	>(null);
@@ -26,11 +22,11 @@ export const Board: React.FC = () => {
 		(state: RootState) => state.boards.selected
 	);
 
-	useEffect(() => {
-		setTimeout(() => {
-			embla?.reInit();
-		}, 250);
+	const isOpen = useSelector(
+		(state: RootState) => state.dashboardAside.staggered
+	);
 
+	useEffect(() => {
 		for (let column of selectedBoard?.columns ?? []) {
 			for (let task of column.task) {
 				if (task.id === selectedTask?.id) setSelectedTask(task);
@@ -38,23 +34,47 @@ export const Board: React.FC = () => {
 		}
 	}, [selectedBoard]);
 
-	useEffect(() => {
-		let timeoutId: number;
-		const onResize = () => {
-			if (timeoutId) clearTimeout(timeoutId);
-			timeoutId = window.setTimeout(() => {
-				embla?.reInit();
-			}, 250);
-		};
+	const outer = useRef<HTMLDivElement>(null);
+	const [rightBound, setRightBound] = useState(0);
+	const [{ x }, api] = useSpring(() => ({ x: 0 }));
 
-		window.addEventListener("resize", onResize);
-		return () => window.removeEventListener("resize", onResize);
-	}, [embla]);
+	useEffect(() => {
+		setRightBound(
+			(outer?.current?.clientWidth ?? 0) -
+				(outer.current?.children[0]?.scrollWidth ?? 0)
+		);
+	}, [selectedBoard, isOpen]);
+
+	// Set the drag hook and define component movement based on gesture data
+	const bind = useDrag(
+		({ offset: [mx], target, cancel }) => {
+			if (
+				target instanceof HTMLElement &&
+				(target instanceof HTMLButtonElement ||
+					target.parentElement instanceof HTMLButtonElement)
+			) {
+				cancel();
+				return;
+			}
+			api.start({ x: mx, config: { frequency: 0.1, damping: 0.8 } });
+		},
+		{
+			bounds: {
+				left: rightBound - 150,
+				right: 0,
+			},
+			rubberband: 0.8,
+		}
+	);
 
 	return (
 		<>
-			<div className="overflow-hidden w-full h-full pr-[50px]" ref={ref}>
-				<div className="bg-grey-100 dark:bg-grey-600 p-6 h-full grid grid-flow-col gap-6 auto-cols-[280px]">
+			<div ref={outer} className="overflow-hidden w-full h-full pr-[50px]">
+				<animated.div
+					{...bind()}
+					style={{ x }}
+					className="bg-grey-100 z-40 touch-none dark:bg-grey-600 p-6 h-full grid grid-flow-col gap-6 auto-cols-[280px]"
+				>
 					{selectedBoard?.columns.map((column) => {
 						return (
 							<Column
@@ -85,7 +105,7 @@ export const Board: React.FC = () => {
 					>
 						+ New Column
 					</button>
-				</div>
+				</animated.div>
 			</div>
 
 			<Modal
